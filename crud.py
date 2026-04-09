ed4e4f4e365abc248a7e000cce5ae0ae9fcf714e
Salatungujar <162285077+Salatungujar@users.noreply.github.com>
2026-04-09 22:27:38 +0530

    Implement CRUD operations on marks list

---
+++ crud.py
@@ -0,0 +1,14 @@
+marks = [80, 70, 90]
+
+marks.append(75)      # add new value
+marks[1] = 50        # update value at index 1
+marks.remove(90)      # remove value 90
+print(marks)
+
+# update example (change value)
+marks[0] = 85
+print(marks)
+
+# delete example
+del marks[1]
+print(marks)
